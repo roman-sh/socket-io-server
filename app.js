11c5607919c42f09f5d43101a21c832d043e8cde
Roman Shmigelsky <shmigelsky@gmail.com>
2018-09-29 02:24:43 +0300

    weather provider changed

--- app.js
+++ app.js
@@ -13,24 +13,32 @@ const server = http.createServer(app);
 
 const io = socketIo(server); // < Interesting!
 
-io.on("connection", socket => {
-  console.log("New client connected");
-  setInterval(
-    () => getApiAndEmit(socket),
-    10000
-  );
-  socket.on("disconnect", () => console.log("Client disconnected"));
+let interval;
+io.on('connection', socket => {
+    console.log('New client connected');
+    if (interval) {
+        clearInterval(interval);
+    }
+    interval = setInterval(() => getApiAndEmit(socket), 10000);
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    });
 });
 
+const city = 'Florence',
+    apiKey = '5ce81ea3350724b0431026e887e45544';
+
 const getApiAndEmit = async socket => {
-  try {
-    const res = await axios.get(
-      "https://api.darksky.net/forecast/0aca4f64a5c623b30a0f294726b6d650/43.7695,11.2558"
-    ); // Getting the data from DarkSky
-    socket.emit("FromAPI", res.data.currently.temperature); // Emitting a new message. It will be consumed by the client
-  } catch (error) {
-    console.error(`Error: ${error.code}`);
-  }
+    try {
+        const res = await axios.get(
+            //"https://api.darksky.net/forecast/0aca4f64a5c623b30a0f294726b6d650/43.7695,11.2558"
+            `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+        ); // Getting the data from DarkSky
+        //socket.emit("FromAPI", res.data.currently.temperature); // Emitting a new message. It will be consumed by the client
+        socket.emit("FromAPI", res.data.main.temp);
+    } catch (error) {
+        console.error(`Error: ${error}`);
+    }
 };
 
-server.listen(port,() => console.log(`Listening on port ${port}`));
+server.listen(port, () => console.log(`Listening on port ${port}`));
